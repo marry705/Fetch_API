7ca139578cea1dc8f674042855ccbb4c248a5cf1
Marry705 <marry705chilo@gmail.com>
2019-05-26 17:57:06 +0300

    feat: added eventlisteners for all buttons and function for getting information from web

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 function app() {
+  function getPosts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then((ansver) => {
+        let output = '<h2 class="mb-4">Posts</h2>';
+        ansver.forEach((post) => {
+          output += `
+            <div class="card mb-3">
+              <h3>${post.title}</h3>
+              <p>${post.body}</p>
+            </div>
+          `;
+        });
+        document.getElementById('output').innerHTML = output;
+      });
+  }
+
+  function getText() {
+    fetch('sample.txt')
+      .then(res => res.text())
+      .then((ansver) => {
+        document.getElementById('output').innerHTML = ansver;
+      });
+  }
+
+
   document.getElementById('getText').addEventListener('click', () => {
     getText();
   });
